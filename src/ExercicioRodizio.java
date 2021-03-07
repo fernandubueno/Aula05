0b1d9a4df808a2509d2a25f27f865de7500e1028
Fernando Bueno <fernando2583@gmail.com>
2021-03-06 23:12:15 -0300

    ExercicioRodizio resolvido

--- src/ExercicioRodizio.java
+++ src/ExercicioRodizio.java
@@ -1,38 +1,57 @@
 import java.util.Scanner;
 
 public class ExercicioRodizio {
-
 	public static void main(String[] args) {
-		String nome, setor, transporte;
-		int placa;
-		
 		Scanner leitor = new Scanner(System.in);
 		
-		System.out.println("Informe o nome do funcionário:");
-		nome = leitor.next();
+		String nome, setor;
+		int meioDeTransporte, placa;
 		
-		System.out.println("Informe em qual setor o colaborador atua:");
-		setor = leitor.next();
-		
-		System.out.println("Informe o meio de transporte utilizado pelo funcionário:");
-		transporte = leitor.next();
-		
-		for(transporte == .equalsIgnoreCase("carro") {
-			System.out.println("Informe o final da placa do veículo:");
-			placa = leitor.nextInt();
+		for(int i=1;i<=150;i++) {
+			System.out.println("Por favor, informe o nome do " + i + "º funcionário");
+			nome = leitor.next();
+			System.out.println("Por favor, informe o setor do " + i + "º funcionário");
+			setor = leitor.next();
 			
-			if(placa == 1||2) {
-				System.out.println("Seu rodízio é na segunda-feira.");
-			}else(placa == 3||4){
-				System.out.println("Seu rodízio é na terça-feira.");
-			}else(placa == 5||6){
-				System.out.println("Seu rodízio é na quarta-feira.");
-			}else(placa == 7||8){
-				System.out.println("Seu rodízio é na quinta-feira.");
-			}else(placa == 9||0){
-				System.out.println("Seu rodízio é na sexta-feira.");
+			System.out.println("Digite 1 se o funcionário utiliza o CARRO e 2 se o funcionário utiliza ÔNIBUS");
+			meioDeTransporte = leitor.nextInt();
+			
+			if(meioDeTransporte == 1) {
+				System.out.println("Por favor, informe o último dígito da placa do carro do funcionário");
+				placa = leitor.nextInt();
+				
+				switch(placa) {
+				case 1: 
+				case 2:
+					System.out.println("Atenção, " + nome + " do setor "+ setor + ", seu dia de rodízio é segunda-feira");
+					break;
+				case 3:
+				case 4:
+					System.out.println("Atenção, " + nome + " do setor "+ setor + ", seu dia de rodízio é terça-feira");
+					break;
+				case 5:
+				case 6:
+					System.out.println("Atenção, " + nome + " do setor "+ setor + ", seu dia de rodízio é quarta-feira");
+					break;
+				case 7:
+				case 8:
+					System.out.println("Atenção, " + nome + " do setor "+ setor + ", seu dia de rodízio é quinta-feira");
+					break;
+				case 9:
+				case 0:
+					System.out.println("Atenção, " + nome + " do setor "+ setor + ", seu dia de rodízio é sexta-feira");
+					break;
+				}
+			}else {
+				if (meioDeTransporte ==2) {
+					System.out.println("Atenção, " + nome + " do setor "+ setor + ", o rodízio municipal não se aplica ao transporte de onibus");
+					
+				}else {
+					System.out.println("Meio de transporte inválido");
+				}
 			}
 		}
+		leitor.close();
 
 	}
 
